034b857eb40d382b50a1bbec055cbfaaa5d2ed2f
Julian Brasse <julian.brasse@gmail.com>
2026-02-20 17:17:34 +0100

    Version 0.2.0

--- scripts/website.js
+++ scripts/website.js
@@ -1,12 +1,14 @@
 import { supabase } from "https://julianbrasse.github.io/zurichpride/scripts/supabase.js";
 
-let styleCSS;
-let styleHTML;
-let headHTML;
-let bodyHTML;
-let headerHTML;
-let footerHTML
-let indexHTML;
+let pageMetadata;
+let pageContent;
+
+let styleMetadata;
+let headerContent;
+let footerContent;
+let indexContent;
+let indexMetadata;
+
 let demonstrationHTML;
 let festivalHTML;
 let kalenderHTML;
@@ -23,44 +25,46 @@ async function getElements() {
 		console.error(error);
 	} else {
 		let elements = Object.fromEntries(data.map(row => [row.name, row.production]));
-		headHTML = elements.headHTML;
-		headerHTML = elements.headerHTML;
-		footerHTML = elements.footerHTML;
-		indexHTML = elements.indexHTML;
+		styleMetadata = elements.style_metadata;
+		headerContent = elements.header_content;
+		footerContent = elements.footer_content;
+		indexContent = elements.index_content;
+		indexMetadata = elements.index_metadata;
+
 		demonstrationHTML = elements.demonstrationHTML;
 		festivalHTML = elements.festivalHTML;
 		kalenderHTML = elements.kalenderHTML;
 		communityHTML = elements.communityHTML;
 		vereinHTML = elements.vereinHTML;
 		kontaktHTML = elements.kontaktHTML;
-
-		styleCSS = elements.styleCSS;
-		styleHTML = `<style>${styleCSS}</style>
 	}
 }
-
 
 async function loadPage() {
 	const parameters = new URLSearchParams(window.location.search);
 	await getElements();
 
-	let contentHTML = indexHTML;
-	if (parameters.has('demonstration')) { contentHTML = demonstrationHTML; }
-	else if (parameters.has('festival')) { contentHTML = festivalHTML; }
-	else if (parameters.has('kalender')) { contentHTML = kalenderHTML; }
-	else if (parameters.has('community')) { contentHTML = communityHTML; }
-	else if (parameters.has('verein')) { contentHTML = vereinHTML; }
-	else if (parameters.has('kontakt')) { contentHTML = kontaktHTML; }
+	let pageMetadata = indexMetadata;
+	let pageContent = indexContent;
+	if (parameters.has('demonstration')) { pageContent = demonstrationHTML; }
+	else if (parameters.has('festival')) { pageContent = festivalHTML; }
+	else if (parameters.has('kalender')) { pageContent = kalenderHTML; }
+	else if (parameters.has('community')) { pageContent = communityHTML; }
+	else if (parameters.has('verein')) { pageContent = vereinHTML; }
+	else if (parameters.has('kontakt')) { pageContent = kontaktHTML; }
 
-	bodyHTML = `
-		<header>${headerHTML}</header>
-		${contentHTML}
-		<footer>${footerHTML}</footer>
+	let headContent = `
+		${pageMetadata}
+		${styleMetadata}
+	`
+	let bodyContent = `
+		${headerContent}
+		${pageContent}
+		${footerContent}
 	`
 
-	document.body.innerHTML = bodyHTML;
-	document.head.insertAdjacentHTML(`beforeend`, headHTML);
-	document.head.insertAdjacentHTML(`beforeend`, styleHTML);
+	document.head.insertAdjacentHTML(`beforeend`, headContent);
+	document.body.insertAdjacentHTML(`beforeend`, bodyContent);
 }
 
 loadPage();
